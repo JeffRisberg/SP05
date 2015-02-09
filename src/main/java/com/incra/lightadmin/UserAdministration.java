d28f97aa7c1e87bffb63f670b23b95415fa4614a
Jeff Risberg <jrisberg@aol.com>
2015-02-09 14:28:55 -0800

    integrated LightAdmin for User entity

---
+++ src/main/java/com/incra/lightadmin/UserAdministration.java
@@ -0,0 +1,34 @@
+package com.incra.lightadmin;
+
+import com.incra.models.User;
+import org.lightadmin.api.config.AdministrationConfiguration;
+import org.lightadmin.api.config.builder.EntityMetadataConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.FieldSetConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.ScreenContextConfigurationUnitBuilder;
+import org.lightadmin.api.config.unit.EntityMetadataConfigurationUnit;
+import org.lightadmin.api.config.unit.FieldSetConfigurationUnit;
+import org.lightadmin.api.config.unit.ScreenContextConfigurationUnit;
+
+/**
+ * Created by Jeffrey Risberg on 2/9/2015.
+ */
+public class UserAdministration extends AdministrationConfiguration<User> {
+
+
+    public EntityMetadataConfigurationUnit configuration(EntityMetadataConfigurationUnitBuilder configurationBuilder) {
+        return configurationBuilder.nameField("firstName").build();
+    }
+
+
+    public ScreenContextConfigurationUnit screenContext(ScreenContextConfigurationUnitBuilder screenContextBuilder) {
+        return screenContextBuilder
+                .screenName("Users Administration").build();
+    }
+
+    public FieldSetConfigurationUnit listView(final FieldSetConfigurationUnitBuilder fragmentBuilder) {
+        return fragmentBuilder
+                .field("firstName").caption("First Name")
+                .field("lastName").caption("Last Name")
+                .build();
+    }
+}
