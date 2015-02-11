2cdad7b3f0ed0f77c2c05e4c57ead7a4e036a9cf
Jeff Risberg <jrisberg@aol.com>
2015-02-10 20:30:14 -0800

    added security to dashboard, customized strings

---
+++ src/main/java/com/incra/lightadmin/DashboardConfiguration.java
@@ -0,0 +1,8 @@
+package com.incra.lightadmin;
+
+// TODO: max: @Administration( type = AdministrationType.Dashboard ) //?
+// TODO: max: @Dashboard //?
+@SuppressWarnings( "unused" )
+public class DashboardConfiguration {
+
+}
